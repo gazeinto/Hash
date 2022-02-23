7b72ed7615c31b5d1643c7cc38c3ff5f94fbce04
ASUS <ASUS@DESKTOP-R11PSVD>
2022-02-24 02:04:40 +0900

    페이징 처리 여러가지 했당

--- hash/src/main/java/kr/or/hash/secondBoard/controller/SecondBoardController.java
+++ hash/src/main/java/kr/or/hash/secondBoard/controller/SecondBoardController.java
@@ -1,11 +1,17 @@
 package kr.or.hash.secondBoard.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.hash.secondBoard.model.service.SecondBoardService;
+import kr.or.hash.secondBoard.model.vo.SecondBoard;
 
 @Controller
 public class SecondBoardController {
@@ -14,14 +20,45 @@ public class SecondBoardController {
 	private SecondBoardService sbService;
 	
 	//게시판 페이지 연결
-	@RequestMapping(value="/secondBoard/SecondBoardPage.do",method = RequestMethod.GET)
-	public String SecondBoardPage(){
+	@RequestMapping(value="/secondBoard/secondBoardPage.do",method = RequestMethod.GET)
+	public ModelAndView SecondBoardPage(ModelAndView mav,
+										@RequestParam(required = false, defaultValue = "1") int currentPage,
+										@RequestParam(required = false, defaultValue = "") String type,
+										@RequestParam(required = false, defaultValue = "default") String keyword){
 		
-		sbService.selectBoard();
+		HashMap<String, Object> map = new HashMap<String, Object>();
 		
-		return "secondBoard/secondBoard";
+		map.put("keyword", keyword);
+		map.put("type", type);
+		
+		map = sbService.selectBoard(currentPage, map);
+		
+		mav.addObject("map",map);
+		mav.setViewName("secondBoard/secondBoard");
+		
+		return mav;
 	}
 	
-	//
+	@RequestMapping(value="/second/secondBoardSelect.do",method = RequestMethod.GET)
+	public ModelAndView secondBoardSelect(@RequestParam int boardNo,
+									ModelAndView mav) {
+		
+		SecondBoard sb = sbService.secondBoardSelect(boardNo);
+		
+		mav.addObject("sb",sb);
+		mav.setViewName("secondBoard/selectBoard");
+		
+		return mav;
+	}
+	
+	//글쓰기
+	@RequestMapping(value="/secondBoard/secondBoardWrite.do")
+	public String secondBoardWrite() {
+	
+		
+		return "secondBoard/boardWrite";
+	}
+	
+	
 	
 }
--- hash/src/main/java/kr/or/hash/secondBoard/model/service/SecondBoardServiceImpl.java
+++ hash/src/main/java/kr/or/hash/secondBoard/model/service/SecondBoardServiceImpl.java
@@ -1,9 +1,13 @@
 package kr.or.hash.secondBoard.model.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.hash.secondBoard.model.dao.SecondBoardDAO;
+import kr.or.hash.secondBoard.model.vo.SecondBoard;
 
 @Service
 public class SecondBoardServiceImpl implements SecondBoardService{
@@ -12,9 +16,28 @@ public class SecondBoardServiceImpl implements SecondBoardService{
 	private SecondBoardDAO sbDAO;
 
 	@Override
-	public void selectBoard() {
+	public HashMap<String, Object> selectBoard(int currentPage, HashMap<String, Object> map) {
 		
-		sbDAO.selectBoard();
+		int recordCountPerPage=5;
+		
+	    ArrayList<SecondBoard> list = sbDAO.selectBoard(recordCountPerPage, currentPage, map);
+	    
+	    int naviCountPerPage = 10;
+	    
+	    String navi = sbDAO.getPageNavi(recordCountPerPage, currentPage, map ,naviCountPerPage);
+	    
+	    map.put("list", list);
+	    map.put("navi", navi);
+	    
+	    return map;
+	    
+		
+	}
+
+	@Override
+	public SecondBoard secondBoardSelect(int boardNo) {
+
+		return sbDAO.secondBoardSelect(boardNo);
 		
 	}
 }
